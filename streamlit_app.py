13c5092158561066634d1c17e97a0ef3a055d165
Sriz <Sriz@users.noreply.github.com>
2023-10-16 13:53:28 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,13 @@
 import streamlit as st
+import guardrails as gd
 from langchain.llms import OpenAI
 from langchain.callbacks import get_openai_callback
+from pydantic import BaseModel
+from rich import print
+from langchain.output_parsers import GuardrailsOutputParser
+from langchain.prompts import PromptTemplate
+from langchain.llms import OpenAI
+
 
 st.title('Code Generator UI')
 
@@ -17,6 +24,14 @@ def get_cb_info(text):
         print(f"Number of Tokens used: {cb.total_tokens}")
         print(f"Amount Spent: ${cb.total_cost}")
 
+class Input(BaseModel):
+    prompt: str
+    
+class Code(BaseModel):
+    """Generated code"""
+    language: str
+    code: str
+    
 with st.form('my_form'):
     language = st.selectbox("Select a language",('Python','Java','C++'))
     prompt_text = st.text_input("Enter your prompt here", '', 5000, 2000)
@@ -26,9 +41,13 @@ with st.form('my_form'):
     if submitted and openai_api_key.startswith('sk-'):
         text = prompt_text +' using '+language
         generate_response(text)
-        with get_openai_callback() as cb:
-            llm = OpenAI()
-            result = llm(text)
-            print(f"Number of Tokens used: {cb.total_tokens}")
-            print(f"Amount Spent: ${cb.total_cost}")
+        guard = gd.Guard.from_pydantic(Code, prompt=text)
+
+        raw_llm_output, validated_output = guard(
+            openai.ChatCompletion.create,
+            model="gpt-3.5-turbo",
+            max_tokens=1024,
+            temperature=0.0,
+        )
+        
 
