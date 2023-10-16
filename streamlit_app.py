f7e68b8d921fecd6f38b959482a3b255f9474a2e
Sriz <Sriz@users.noreply.github.com>
2023-10-16 13:23:36 -0500

    Added a form.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,18 +1,20 @@
 import streamlit as st
 from langchain.llms import OpenAI
 
-st.title('🦜🔗 Quickstart App')
+st.title('Code Generator UI')
 
 openai_api_key = st.sidebar.text_input('OpenAI API Key', type='password')
 
-def generate_response(input_text):
+def generate_response(text):
     llm = OpenAI(temperature=0.7, openai_api_key=openai_api_key)
-    st.info(llm(input_text))
+    st.info(llm(prompt_text))
 
 with st.form('my_form'):
-    text = st.text_area('Enter text:', 'What are the three key pieces of advice for learning how to code?')
+    language = st.selectbox("Select a language",('Python','Java','C++'))
+    prompt_text = st.text_input("Enter your prompt here", '', 5000, 2000)
     submitted = st.form_submit_button('Submit')
     if not openai_api_key.startswith('sk-'):
         st.warning('Please enter your OpenAI API key!', icon='⚠')
     if submitted and openai_api_key.startswith('sk-'):
+        text = prompt_text +' using '+language
         generate_response(text)
